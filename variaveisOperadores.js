9d2d236c5618c393d370f0cb13b151ac284e9e40
CAIO MATOS ALVES BRANCO PEDROSA <caio.pedrosa@a.ucb.br>
2025-03-07 09:13:15 -0300

    Atividade1

---
+++ variaveisOperadores.js
@@ -0,0 +1,27 @@
+// Declaração de variáveis e operações
+const num1 = 10;
+const num2 = 5;
+
+// Soma
+const soma = num1 + num2;
+console.log(`Resultado da soma entre ${num1} e ${num2} é ${soma}`);
+
+// Subtração
+const subtracao = num1 - num2;
+console.log(`Resultado da subtração entre ${num1} e ${num2} é ${subtracao}`);
+
+// Multiplicação
+const multiplicacao = num1 * num2;
+console.log(`Resultado da multiplicação entre ${num1} e ${num2} é ${multiplicacao}`);
+
+// Divisão
+const divisao = num1 / num2;
+console.log(`Resultado da divisão entre ${num1} e ${num2} é ${divisao}`);
+
+// Módulo (resto da divisão)
+const modulo = num1 % num2;
+console.log(`Resultado do módulo entre ${num1} e ${num2} é ${modulo}`);
+
+// Exponenciação
+const exponenciacao = num1 ** num2;
+console.log(`Resultado da exponenciação entre ${num1} e ${num2} é ${exponenciacao}`);
